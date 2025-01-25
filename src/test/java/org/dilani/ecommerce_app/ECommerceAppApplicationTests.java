2282f0fe7464ed5e6fd7d6e4a782c238c53541d3
Dilani_Shyamali <we.shani92@gmail.com>
2025-01-25 20:36:48 +0530

    Initial Commit

---
+++ src/test/java/org/dilani/ecommerce_app/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dilani.ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
